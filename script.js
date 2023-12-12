cbc2620cc0263e51331f235d6b37b7cc86552fc1
Jorge Zimmermann <jorgezimmer95@gmail.com>
2023-12-12 10:39:04 -0300

    agregando cronometro

--- script.js
+++ script.js
@@ -31,6 +31,37 @@ function relojDigital(){
     $fecha.innerHTML = `${showSemana} ${dia}-${mes}-${anio}`
 }
 
+/*cronometro*/
+
+
+    let tiempoRef = Date.now()
+    let iniciarCronometro = false
+    let acumulado = 0
+
+    setInterval(()=> {
+        let cronometro = document.getElementById("cronometro")
+        if(iniciarCronometro){   
+            acumulado = Date.now - tiempoRef
+        }
+        cronometro.innerHTML = acumulado
+
+    }, 1000/60);  
+    
+    function formatearMS(tiempo_ms){
+        let MS = tiempo_ms % 1000
+        let S = Math.floor(((tiempo_ms-MS) / 1000) % 60)
+        let M = Math.floor((S/60) % 60)
+        let H = Math.floor((M/60))
+
+        Number.prototype.ceros = function(n){
+            return (this+"").padStart(n,0)
+        }
+
+        return H.ceros(2)+":"+ M.ceros(2)+":"+H.ceros(2)
+
+    }
+
+
 
 /**agregar tareas */
 
